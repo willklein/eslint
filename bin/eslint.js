abcec71b410be5ee87686927409f57448756ace5
Will Klein <will.klein@gmail.com>
2015-07-12 00:27:31 -0600

    New: Add --autoinit option to auto-generate a config with all passing rules

--- bin/eslint.js
+++ bin/eslint.js
@@ -1,11 +1,24 @@
 #!/usr/bin/env node
 var concat = require("concat-stream"),
+    configAutoInit = require("../lib/config-autoinitializer"),
     configInit = require("../lib/config-initializer"),
     cli = require("../lib/cli");
 
 var exitCode = 0,
     useStdIn = (process.argv.indexOf("--stdin") > -1),
-    init = (process.argv.indexOf("--init") > -1);
+    init = (process.argv.indexOf("--init") > -1),
+    autoInit = (process.argv.indexOf("--autoinit") > -1);
+
+function initializeCallback(err) {
+    if (err) {
+        exitCode = 1;
+        console.error(err.message);
+        console.error(err.stack);
+    } else {
+        console.log("Successfully created .eslintrc file in " + process.cwd());
+        exitCode = 0;
+    }
+}
 
 if (useStdIn) {
     process.stdin.pipe(concat({ encoding: "string" }, function(text) {
@@ -18,16 +31,9 @@ if (useStdIn) {
         }
     }));
 } else if (init) {
-    configInit.initializeConfig(function(err) {
-        if (err) {
-            exitCode = 1;
-            console.error(err.message);
-            console.error(err.stack);
-        } else {
-            console.log("Successfully created .eslintrc file in " + process.cwd());
-            exitCode = 0;
-        }
-    });
+    configInit.initializeConfig(initializeCallback);
+} else if (autoInit) {
+    configAutoInit.initializeConfig(initializeCallback);
 } else {
     exitCode = cli.execute(process.argv);
 }
